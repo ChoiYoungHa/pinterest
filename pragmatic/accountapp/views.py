f3634b2125cabcadda708876aceb1c162fbeb575
YoungHaChoi <askillofgod159@naver.com>
2024-03-29 14:21:43 +0900

    django course 26

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,11 +1,14 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth.models import User
+from django.contrib.auth.views import PasswordChangeView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from .forms import AccountUpdateForm
 from .models import HelloWorld
+
 
 
 # Create your views here.
@@ -40,3 +43,15 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+
+class AccountUpdateView(PasswordChangeView):
+    model = User
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
+
+
+class AccountDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
