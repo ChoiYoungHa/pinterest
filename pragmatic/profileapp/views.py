e932f6ce252659a555f1f17512b303e14a83e8bf
YoungHaChoi <askillofgod159@naver.com>
2024-04-01 15:14:19 +0900

    django course 32 commit

--- pragmatic/profileapp/views.py
+++ pragmatic/profileapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
+from .decorations import profile_ownership_required
 from .forms import ProfileCreationForm
 from .models import Profile
 
@@ -22,4 +24,11 @@ class ProfileCreateView(CreateView):
         return super().form_valid(form)
 
 
-
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    context_object_name = 'target_user'
+    template_name = 'profileapp/update.html'
+    success_url = reverse_lazy('accountapp:hello_world')
