43e726e15e4472f2bb7094a48010c8aa83180eba
YoungHaChoi <askillofgod159@naver.com>
2024-04-08 15:58:11 +0900

    commit

--- pragmatic/profileapp/forms.py
+++ pragmatic/profileapp/forms.py
@@ -3,8 +3,8 @@ from django.forms import ModelForm
 from .models import Profile
 
 
+# 프로파일 폼 추가
 class ProfileCreationForm(ModelForm):
     class Meta:
         model = Profile
         fields = ['image', 'nickname', 'message']
-
